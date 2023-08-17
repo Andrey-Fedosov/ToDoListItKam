91b1f0c7cdd965f1b07b46268a7eb7413488f7a5
Andrey-Fedosov <andrefedosov@gmail.com>
2023-08-17 23:38:54 +0300

    add newstartin values

--- src/MoneyChange.tsx
+++ src/MoneyChange.tsx
@@ -1,15 +1,6 @@
 import React, { useState } from 'react';
-import { Button } from './Button';
-
-type WalletType = {
-  wallet: BanknotPropsType[];
-};
-
-type BanknotPropsType = {
-  banknots: string;
-  value: number;
-  number: string;
-};
+import './App.css';
+// import { Button } from './Button';
 
 // const moneyArray = [
 //   { banknots: 'Dollars', value: 100, number: ' a1234567890' },
@@ -38,48 +29,29 @@ type BanknotPropsType = {
         })
         ); */
 
-export const MoneyChange = (props: WalletType) => {
+export const MoneyChange = () => {
   // let filteredArray: any = [];
-  const [filter, setFilter] = useState('all');
-  const onClickHandler = (btnName: string) => {
-    setFilter(btnName);
-    console.log(filter);
-  };
-
-  let filteredArray = props.wallet.filter((el) => {
-    return el.banknots;
-  });
-
-  if (filter === 'rubles') {
-    return (filteredArray = props.wallet.filter((el) => {
-      return el.banknots === 'RUBLS';
-    }));
-  } else if (filter === 'dollars') {
-    return (filteredArray = props.wallet.filter((el) => {
-      return el.banknots === 'Dollars';
-    }));
-  } else if (filter === 'all') {
-    filteredArray = props.wallet.filter((el) => {
-      return el.banknots;
-    });
-  }
-
+  const [money, setMoney] = useState([
+    { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+    { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+    { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+    { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+    { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+  ]);
   return (
-    <>
-      <ul>
-        {filteredArray.map((b: BanknotPropsType, index: number) => {
-          return (
-            <li key={index}>
-              <span>{`${b.value} ${b.banknots} with number ${b.number}`}</span>
-            </li>
-          );
-        })}
-      </ul>
-      <div style={{ marginLeft: '30px', marginTop: '30px' }}>
-        <Button name="all" callBack={() => onClickHandler('all')} />
-        <Button name="rubles" callBack={() => onClickHandler('rubles')} />
-        <Button name="dollars" callBack={() => onClickHandler('dollars')} />
-      </div>
-    </>
+    <ul>
+      {money.map((objectFromMoneyArr) => {
+        return (
+          <li>
+            <span>{objectFromMoneyArr.banknots}</span>
+            <span>{objectFromMoneyArr.value}</span>
+            <span>{objectFromMoneyArr.number}</span>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
