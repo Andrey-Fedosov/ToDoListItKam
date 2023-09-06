68944bb77e50feb38e58a944e8dcf70c73cb32ac
Andrey-Fedosov <andrefedosov@gmail.com>
2023-09-06 23:04:39 +0300

    feature: implement add new task functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
+import { useState } from "react";
 import "./App.css";
 import { TODOList } from "./components/TODOList";
+import { v1 } from "uuid";
 
 function App() {
   const listName1 = "what to learn";
   const listName2 = "what to learn2";
 
   const tasks1 = [
-    { id: 1, title: "HTML&CSS", isDone: true },
-    { id: 2, title: "JS", isDone: true },
-    { id: 3, title: "ReactRouter", isDone: false },
-    { id: 4, title: "ReactJS", isDone: false },
-    { id: 5, title: "ReactJS", isDone: true },
-    { id: 6, title: "ReactJS", isDone: false },
+    { id: v1(), title: "HTML&CSS", isDone: true },
+    { id: v1(), title: "JS", isDone: true },
+    { id: v1(), title: "ReactRouter", isDone: false },
+    { id: v1(), title: "ReactJS", isDone: false },
+    { id: v1(), title: "ReactJS", isDone: true },
+    { id: v1(), title: "ReactJS", isDone: false },
   ];
 
   const [tasks, setTasks] = useState(tasks1);
 
   const tasks2 = [
-    { id: 1, title: "Hello world", isDone: true },
-    { id: 2, title: "I am Happy", isDone: false },
-    { id: 3, title: "Yo", isDone: false },
-    { id: 4, title: "A-Yo man", isDone: false },
-    { id: 5, title: "Yo-yo", isDone: false },
-    { id: 6, title: "Yo-yo-yo", isDone: false },
+    { id: v1(), title: "Hello world", isDone: true },
+    { id: v1(), title: "I am Happy", isDone: false },
+    { id: v1(), title: "Yo", isDone: false },
+    { id: v1(), title: "A-Yo man", isDone: false },
+    { id: v1(), title: "Yo-yo", isDone: false },
+    { id: v1(), title: "Yo-yo-yo", isDone: false },
   ];
 
-  const deleteTask = (taskId: number) => {
+  const deleteTask = (taskId: string) => {
     return setTasks(tasks.filter((t) => t.id !== taskId));
   };
 
+  const addTask = (title: string) => {
+    const newTask = { id: v1(), title: title, isDone: false };
+    setTasks([newTask, ...tasks]);
+  };
+
   return (
-    <div className="App">
-      <TODOList listName={listName1} taskList={tasks} deleteTask={deleteTask} />
+    <div className="app">
+      <TODOList
+        listName={listName1}
+        taskList={tasks}
+        deleteTask={deleteTask}
+        addTask={addTask}
+      />
     </div>
   );
 }
