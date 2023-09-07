5d33da70a7b85d86883b92238875256af5fe6c19
Andrey-Fedosov <andrefedosov@gmail.com>
2023-09-07 17:22:20 +0300

    feature: add filter functionality, and change status functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { useState } from "react";
 import "./App.css";
-import { TODOList } from "./components/TODOList";
+import { TODOList, filterValuePropsType } from "./components/TODOList";
 import { v1 } from "uuid";
 
 function App() {
   const listName1 = "what to learn";
   const listName2 = "what to learn2";
+  const [filter, setFilter] = useState("all");
+  const [status, setStatus] = useState(false);
 
   const tasks1 = [
     { id: v1(), title: "HTML&CSS", isDone: true },
@@ -28,6 +30,10 @@ function App() {
     { id: v1(), title: "Yo-yo-yo", isDone: false },
   ];
 
+  const changeFilter = (filterValue: filterValuePropsType) => {
+    setFilter(filterValue);
+  };
+
   const deleteTask = (taskId: string) => {
     return setTasks(tasks.filter((t) => t.id !== taskId));
   };
@@ -37,13 +43,43 @@ function App() {
     setTasks([newTask, ...tasks]);
   };
 
+  const changeStatus = (taskId: string, isDone: boolean) => {
+    let [changingTask] = tasks.filter((t) => t.id === taskId);
+    if (changingTask) {
+      changingTask.isDone = isDone;
+      setTasks([...tasks]);
+    }
+  };
+
+  let taskToDisplay = tasks;
+
+  /*   switch (filter) {
+    case "active":
+      taskToDisplay = taskToDisplay.filter((t) => t.isDone === false);
+
+      break;
+    case "complete":
+      taskToDisplay = taskToDisplay.filter((t) => t.isDone === true);
+
+      break;
+  } */
+
+  if (filter === "active") {
+    taskToDisplay = taskToDisplay.filter((t) => t.isDone === false);
+  }
+  if (filter === "complete") {
+    taskToDisplay = taskToDisplay.filter((t) => t.isDone === true);
+  }
+
   return (
     <div className="app">
       <TODOList
         listName={listName1}
-        taskList={tasks}
+        taskList={taskToDisplay}
         deleteTask={deleteTask}
         addTask={addTask}
+        changeFilter={changeFilter}
+        changeStatus={changeStatus}
       />
     </div>
   );
